0a2492dbc19250d1ea8c0306f667c55e0d2da6c2
arshali2774 <arshali.763z@gmail.com>
2024-05-18 05:40:23 +0530

    routing refactor

--- React Advanced/Week-1/02-React Router/01-Routing refactor/q1_starter/src/App.js
+++ React Advanced/Week-1/02-React Router/01-Routing refactor/q1_starter/src/App.js
@@ -1,21 +1,36 @@
-import "./styles.css";
-import { useState } from "react";
+import './styles.css';
+import { useState } from 'react';
 
-import { Navbar } from "./components/Navbar";
-import { Home } from "./pages/Home";
-import { List } from "./pages/List";
-import { Contact } from "./pages/Contact";
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { List } from './pages/List';
+import { Contact } from './pages/Contact';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 export default function App() {
-  const [page, setPage] = useState("home");
+  const [page, setPage] = useState('home');
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/list',
+      element: <List />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+  ]);
   return (
-
-    <div className="App">
-      <Navbar/>
+    <div className='App'>
+      <Navbar />
       {/*Refactor this to use routes here */}
-      {page === "home" && <Home />}
-      {page === "list" && <List />}
-      {page === "contact" && <Contact />}
+      {/* {page === 'home' && <Home />}
+      {page === 'list' && <List />}
+      {page === 'contact' && <Contact />} */}
+      <RouterProvider router={router} />
     </div>
   );
 }
