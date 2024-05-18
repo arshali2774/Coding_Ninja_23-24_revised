fe42399177559e93a5d15582d01c082a63e02127
arshali2774 <arshali.763z@gmail.com>
2024-05-18 05:59:17 +0530

    predefined routes

---
+++ React Advanced/Week-1/02-React Router/02-Predefned Routes/q2_starter/src/App.js
@@ -0,0 +1,33 @@
+import './styles.css';
+// import necessary components here
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { List } from './pages/List';
+import { Contact } from './pages/Contact';
+
+export default function App() {
+  // debug the code below to create predefined route
+  const routes = createRoutesFromElements(
+    <>
+      <Route path='/' element={<Home />} />
+      <Route path='/list' element={<List />} />
+      <Route path='/contact' element={<Contact />} />
+    </>
+  );
+
+  const router = createBrowserRouter(routes);
+
+  return (
+    <div className='App'>
+      <Navbar />
+      <RouterProvider router={router} />
+    </div>
+  );
+}
